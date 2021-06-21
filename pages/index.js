20cc5602de7c04cf1f3ff0185da9eb26453ea973
Jeff Titak <jtitak06@yahoo.com>
2021-06-21 00:41:55 -0500

    3rd version

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 const editButton = document.querySelector('.profile__edit');
 const closeButton = document.querySelector('.popup__button');
-const saveButton = document.querySelector('.form__button');
 const form = document.querySelector('.form');
 const nameInput = document.querySelector('.form__input_type_name');
 const aboutInput = document.querySelector('.form__input_type_about');
@@ -11,25 +10,17 @@ const popup = document.querySelector('.popup');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   
-  const nameInput = document.querySelector('.form__input_type_name');
-  const aboutInput = document.querySelector('.form__input_type_about');
-  
-  const profileName = document.querySelector('.profile__name');
-  const profileAbout = document.querySelector('.profile__description');
-  
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
   
                       });
 
 editButton.addEventListener('click', (a)=> {
-  const popup = document.querySelector('.popup');
   popup.classList.add('popup__open');
   
 } );
 
 closeButton.addEventListener('click', (a)=> {
-  const popup = document.querySelector('.popup');
   popup.classList.remove('popup__open');
   
 } );
